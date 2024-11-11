5d20201159e55b82c831daf7f0aca5630503767d
dquansah <dquansah@tapnhop.com>
2024-11-11 16:26:56 +0000

    Implement UDP socket communication in SendScreen and RecieveScreen

--- app/screens/RecieveScreen.tsx
+++ app/screens/RecieveScreen.tsx
@@ -1,10 +1,54 @@
 import {Text, View} from 'react-native';
-import React from 'react';
+import React, {useRef} from 'react';
+import {Button} from 'react-native-paper';
+import dgram from 'react-native-udp';
+import UdpSocket from 'react-native-udp/lib/types/UdpSocket';
 
 const RecieveScreen = () => {
+  const socketRef = useRef<UdpSocket | null>(null);
+
+  const startListening = () => {
+    console.log('start listening');
+
+    //udp socket to do the listening
+    const socket = dgram.createSocket({
+      type: 'udp4',
+      reusePort: true,
+      debug: true,
+    });
+    socketRef.current = socket;
+
+    //bind the socket to the port
+    socket.bind(3000, () => {
+      console.log('binded');
+
+      //listen for messages
+      socket.setBroadcast(true);
+    });
+
+    //handle incoming messages
+    socket.on('message', (msg, rinfo) => {
+      console.log(`Message: ${msg} from ${rinfo.address}:${rinfo.port}`);
+    });
+
+    // Error handling
+    socket.on('error', err => {
+      console.error(`Error: ${err.message}`);
+      socket.close();
+    });
+  };
+
+  const stopListening = () => {
+    if (socketRef.current) {
+      socketRef.current.close();
+      socketRef.current = null;
+    }
+  };
   return (
     <View>
       <Text style={{}}>RecieveScreen</Text>
+      <Button onPress={startListening}>Start Listening</Button>
+      <Button onPress={stopListening}>Stop Listening</Button>
     </View>
   );
 };
--- app/screens/SendScreen.tsx
+++ app/screens/SendScreen.tsx
@@ -1,14 +1,98 @@
-import {Text, View} from 'react-native';
-import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import React, {useEffect, useRef} from 'react';
+import {NetworkInfo} from 'react-native-network-info';
+import dgram from 'react-native-udp';
+import {Button} from 'react-native-paper';
+import UdpSocket from 'react-native-udp/lib/types/UdpSocket';
+
+// Broadcast every 2 seconds
+const BROADCAST_INTERVAL = 2000;
+const BROADCAST_PORT = 3000;
+const BROADCAST_MESSAGE = JSON.stringify({id: 'device123', name: 'MyDevice'});
 
 const SendScreen = () => {
+  const socketRef = useRef<UdpSocket | null>(null);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  const startListening = async () => {
+    console.log('listenting');
+
+    const ipAddress = await NetworkInfo.getIPAddress();
+    console.log('IP Address: ', ipAddress);
+    if (!ipAddress) {
+      return;
+    }
+
+    // broadcast IP (usually ending in 255)
+    const broadcastAddress = `${ipAddress
+      .split('.')
+      .slice(0, 3)
+      .join('.')}.255`;
+    console.log('Broadcasting to: ', broadcastAddress);
+
+    // initialising udp socket
+    const socket = dgram.createSocket({
+      type: 'udp4',
+      reusePort: true,
+      debug: true,
+    });
+    socketRef.current = socket;
+
+    socket.bind(BROADCAST_PORT, () => {
+      console.log('binded');
+      intervalRef.current = setInterval(() => {
+        socket.send(
+          BROADCAST_MESSAGE,
+          0,
+          BROADCAST_MESSAGE.length,
+          BROADCAST_PORT,
+          broadcastAddress,
+          err => {
+            if (err) {
+              console.log('Broadcast Error: ', err);
+            } else {
+              console.log('Broadcasted: ', BROADCAST_MESSAGE);
+            }
+          },
+        );
+      }, BROADCAST_INTERVAL);
+    });
+  };
+
+  const stopListening = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+
+    if (socketRef.current) {
+      socketRef.current.close();
+      socketRef.current = null;
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      stopListening();
+    };
+  }, []);
+
   return (
-    <View>
-      <Text>SendScreen</Text>
+    <View style={styles.container}>
+      <Button onPress={startListening}>Start broadcasting</Button>
+      <Button onPress={stopListening} mode="outlined">
+        Stop braodcasting
+      </Button>
     </View>
   );
 };
 
 export default SendScreen;
 
-// const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
